3c6ea2112e1ca78d051740fbe7b88bbd4f1c3086
Anderson Ornelas <63083020+ornelas91@users.noreply.github.com>
2021-05-30 17:44:00 -0300

    Adicionado

---
+++ js/voltartopo.js
@@ -0,0 +1,17 @@
+$ (function() {
+    $('.back-to-top').hide () ;
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn () ;
+        }else{
+            $('.back-to-top').fadeOut () ;
+        }
+    }) ;
+
+    $('.back-to-top').click (function() {
+        $('html, body').animate ({
+            scrollTop: 0
+        }, 800) ;
+    }) ;
+}) ;
